55c270947586a0b71f7ff0bb5376f8ed135ba2c6
Kishore Pendyala <kishorekpendyala@yahoo.com>
2022-07-15 11:24:29 -0400

    Update setup-bitcoin.sh

--- setup-bitcoin.sh
+++ setup-bitcoin.sh
@@ -1,10 +1,11 @@
 
 cd /satoshi
 
-mkdir bin
+mkdir -p /satoshi/bin
 
 cp /satoshi/bitcoin-scripts/*.sh /satoshi/bin
 
+cd /satoshi/bin
 ln -s /satoshi/bitcoin/src/bitcoind bitcoind
 ln -s /satoshi/bitcoin/src/bitcoin-cli bitcoin-cli
 ln -s /satoshi/bitcoin/src/bitcoin-tx bitcoin-tx 
