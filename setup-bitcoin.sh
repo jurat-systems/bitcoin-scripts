9cfa8f7b22cfec0929cddd1616b69870fbe61e7d
Kishore Pendyala <kishorekpendyala@yahoo.com>
2022-07-17 18:14:02 -0400

    Update setup-bitcoin.sh

--- setup-bitcoin.sh
+++ setup-bitcoin.sh
@@ -14,7 +14,7 @@ cp /satoshi/bitcoin-scripts/bitcoin.conf /satoshi
 
 current_host_ip=`hostname | sed 's/ip-*//g' | sed 's/-/./g'`
 
-sed -i "s/172.31.11.40/$current_host_ip/g' bitcoin.conf
+sed -i "s/172.31.11.40/$current_host_ip/g" /satoshi/bitcoin.conf
 
 mkdir -p /satoshi/data/blocks
 
